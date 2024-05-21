96eb408ac575f3a1bd6b712c59fe92b71181857c
Andres_Tech <159641321+TechBlood@users.noreply.github.com>
2024-05-20 23:13:23 -0600

    Esto va dentro de una carpeta para visual
    Esto va todo en una carpeta para el visual que se llame "MayaCine"

---
+++ app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+  
---
+++ app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+     
+    { path: '', component:LoginComponent},
+    { path:'WelcomeComponent', component:WelcomeComponent},
+];
+
